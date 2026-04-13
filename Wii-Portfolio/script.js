2dabe4cbda06ad758773ef79d36b24659288b99b
Alvaro Lozano de Osma <74834169+Alozanodeosma@users.noreply.github.com>
2026-04-13 16:45:46 +0200

    animaciones click en las cards

--- Wii-Portfolio/script.js
+++ Wii-Portfolio/script.js
@@ -41,7 +41,37 @@ const cards = document.querySelectorAll(".cards");
 cards.forEach(card => {
   card.addEventListener("mouseenter", () => {
     channelHover.currentTime = 0;
-    channelHover.volume = 0.2;
+    channelHover.volume = 0.1;
     channelHover.play()
+});
+});
+
+//Card fullscreen transition management
+const blackBackground = document.getElementById("black-background");
+cards.forEach(card => {
+  card.addEventListener("click", () => {
+    const first = card.getBoundingClientRect();
+    card.classList.toggle("cards-fullscreen");
+
+    const last = card.getBoundingClientRect();
+
+    const deltaX = first.left - last.left;
+    const deltaY = first.top - last.top;
+    const scaleX = first.width / last.width;
+    const scaleY = first.height / last.height;
+
+    card.style.transition = "none";
+    card.style.transform = `translate(${deltaX}px, ${deltaY}px) scale(${scaleX}, ${scaleY})`;
+    card.style.transformOrigin = "top left";
+    blackBackground.style.backgroundColor = card.classList.contains("cards-fullscreen") ? "transparent" : "black";  
+
+    requestAnimationFrame(() => {
+      requestAnimationFrame(() => {
+        card.style.transition = "transform 0.5s ease-out, background-color 0.6s ease-out";
+        card.style.transform = "";
+        blackBackground.style.backgroundColor = card.classList.contains("cards-fullscreen") ? "black" : "transparent";  
+        blackBackground.style.transition = "background-color 0.5s ease-out";
+    });
+  });
 });
 });
